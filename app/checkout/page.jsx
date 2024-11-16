57cafaafe302a6902020d5d9a133d1a97ce1a13b
CamloD <sergado9@gmail.com>
2024-11-16 16:37:52 -0500

    Version 2.1.1

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,3 +1,4 @@
+// components/Checkout.jsx
 'use client'
 
 import { useState, useMemo } from 'react'
@@ -5,10 +6,12 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useLanguage } from '../contexts/LanguageContext'
+import { useCart } from '../contexts/CartContext' // Importar el hook del contexto
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 
-export default function Checkout({ cart, onCheckoutComplete }) {
+export default function Checkout() {
   const { t } = useLanguage()
+  const { cart, onCheckoutComplete } = useCart() // Obtener cart y onCheckoutComplete del contexto
   const [isOpen, setIsOpen] = useState(false)
   const [formData, setFormData] = useState({
     name: '',
